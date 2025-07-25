cb4a084aea62af38ac3d14dff9bc248956aa28f6
Muhammad Izza Alfiansyah <iansyah724@gmail.com>
2025-07-25 16:58:14 +0700

    updating configuration for relative number

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -8,6 +8,8 @@ local opt = vim.opt
 vim.g.lazyvim_php_lsp = "intelephense"
 vim.g.lazyvim_python_lsp = "pyright"
 vim.g.lazyvim_python_ruff = "ruff"
+vim.g.lazyvim_picker = "fzf"
 
 opt.wrap = true
-vim.g.lazyvim_picker = "fzf"
+opt.relativenumber = false
+opt.number = true
