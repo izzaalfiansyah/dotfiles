5d64d2b41dd56a1fe24306c50e457167e3466924
Muhammad Izza Alfiansyah <iansyah724@gmail.com>
2025-07-30 16:22:54 +0700

    updating wezterm theme

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,73 +1,67 @@
 local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
+local scheme = "Catppuccin Macchiato"
+local tabline = wezterm.plugin.require("https://github.com/michaelbrusegard/tabline.wez")
 
-config.color_scheme = "Catppuccin Macchiato"
+config.color_scheme = scheme
 
-config.font = wezterm.font_with_fallback({
-	{ family = "JetBrainsMonoNL Nerd Font Mono" },
-	{ family = "Hack Nerd Font" },
-	{ family = "Fira Code" },
-})
 config.font_size = 12
-
-config.enable_tab_bar = true
-config.use_fancy_tab_bar = false
-config.tab_bar_at_bottom = true
-config.tab_max_width = 30
+config.font = wezterm.font_with_fallback({
+	{ family = "JetBrainsMonoNL Nerd Font Mono", weight = "DemiBold" },
+	{ family = "Hack Nerd Font", weight = "DemiBold" },
+	{ family = "Fira Code", weight = "DemiBold" },
+})
 
 config.window_decorations = "RESIZE"
 
 config.window_background_opacity = 0.75
 config.macos_window_background_blur = 15
+config.tab_bar_at_bottom = true
 
-local function tab_title(tab_info)
-	local title = tab_info.tab_title
+tabline.setup({
+	options = {
+		icons_enabled = true,
+		theme = scheme,
+		tabs_enabled = true,
+		theme_overrides = {},
+		section_separators = {
+			left = wezterm.nerdfonts.pl_left_hard_divider,
+			right = wezterm.nerdfonts.pl_right_hard_divider,
+		},
+		component_separators = {
+			left = wezterm.nerdfonts.pl_left_soft_divider,
+			right = wezterm.nerdfonts.pl_right_soft_divider,
+		},
+		tab_separators = {
+			left = wezterm.nerdfonts.pl_left_hard_divider,
+			right = wezterm.nerdfonts.pl_right_hard_divider,
+		},
+	},
+	sections = {
+		tabline_a = { "window" },
+		tabline_b = { "workspace" },
+		tabline_c = { " " },
+		tab_active = {
+			"index",
+			{ "process", padding = { left = 0, right = 1 } },
+			"|",
+			"cwd",
+		},
+		tab_inactive = {
+			"index",
+			{ "cwd", padding = { left = 0, right = 1 } },
+		},
+		tabline_x = { "ram", "cpu" },
+		tabline_y = { "datetime", "battery" },
+		tabline_z = { "domain" },
+	},
+	extensions = {},
+})
 
-	if title and #title > 0 then
-		return title
-	end
-
-	return tab_info.active_pane.title
-end
-
-local SOLID_LEFT_ARROW = wezterm.nerdfonts.pl_right_hard_divider
-local SOLID_RIGHT_ARROW = wezterm.nerdfonts.pl_left_hard_divider
-
-wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
-	local edge_background = "#1a1b26"
-	local background = "#141C24"
-	local foreground = "#BC9AF7"
-
-	if tab.is_active then
-		background = "#23283B"
-		foreground = "#7DCFFF"
-	elseif hover then
-		background = "#23283B"
-		foreground = "#E0AF68"
-	end
-
-	local edge_foreground = background
-
-	local title = tab_title(tab)
-
-	title = wezterm.truncate_right(title, max_width - 2)
-
-	return {
-		{ Background = { Color = edge_background } },
-		{ Foreground = { Color = edge_foreground } },
-		{ Text = SOLID_LEFT_ARROW },
-		{ Background = { Color = background } },
-		{ Foreground = { Color = foreground } },
-		{ Text = title },
-		{ Background = { Color = edge_background } },
-		{ Foreground = { Color = edge_foreground } },
-		{ Text = SOLID_RIGHT_ARROW },
-	}
-end)
+tabline.apply_to_config(config)
 
 config.mouse_bindings = {
-	-- Ctrl-click will open the link under the mouse cursor
 	{
 		event = { Up = { streak = 1, button = "Left" } },
 		mods = "CTRL",
@@ -134,13 +128,5 @@ config.mouse_bindings = {
 -- 		action = wezterm.action.AdjustPaneSize({ "Down", 1 }),
 -- 	},
 -- }
-
--- change term using kitty. run command below:
--- tempfile=$(mktemp) \
---   && curl -o $tempfile https://raw.githubusercontent.com/kovidgoyal/kitty/refs/heads/master/terminfo/kitty.terminfo \
---   && tic -x -o ~/.terminfo $tempfile \
---   && rm $tempfile
--- config.term = "xterm-kitty"
--- config.enable_kitty_graphics = true
 
 return config
