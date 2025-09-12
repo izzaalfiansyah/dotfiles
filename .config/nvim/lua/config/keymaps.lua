013508fadd547846fac74d27dd33f4d7839f7bb2
Muhammad Izza Alfiansyah <iansyah724@gmail.com>
2025-09-12 10:10:04 +0700

    adjust keybinding

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,38 +1,16 @@
 local opts = { noremap = true, silent = true }
 
--- Ignore dotfiles on telescope
--- vim.keymap.set("n", "<leader>ff", function() require("telescope.builtin").find_files({
---     hidden = true,
---     no_ignore = true,
---   })
--- end, { desc = "Find files (+dotfiles, +ignored)" })
---
--- vim.keymap.set("n", "<leader><space>", function()
---   require("telescope.builtin").find_files({
---     hidden = true,
---     no_ignore = true,
---   })
--- end, { desc = "Find files (include dotfiles & ignored)" })
-
--- Exit insert, visual, and terminal mode using jk
+-- Exit from a mode
 vim.keymap.set("i", "jk", "<Esc>", { desc = "Exit insert mode with jk" })
 vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]], { desc = "Exit terminal mode with qq", noremap = true, silent = true })
-
--- Keymap for set attribute from object and set array on php
--- vim.keymap.set("i", ",,", "->", { desc = "Type -> in insert mode", noremap = true })
--- vim.keymap.set("i", ";;", "=>", { desc = "Type => in insert mode", noremap = true })
 
 -- Selection
 vim.keymap.set("n", "<C-a>", "ggVG")
 
 -- Save File
--- vim.keymap.set("n", "<leader>fs", vim.cmd.write, { desc = "Save file" })
 vim.keymap.set("n", "<C-s>", vim.cmd.write, { desc = "Save file" })
 vim.keymap.set("i", "<C-s>", vim.cmd.write, { desc = "Save file" })
 vim.keymap.set("v", "<C-s>", vim.cmd.write, { desc = "Save file" })
-
--- Close Tab
--- vim.keymap.set("n", "<leader>fw", ":bd<CR>", { desc = "Delete buffer" })
 
 -- Set File Type
 vim.keymap.set("n", "<leader>sf", function()
@@ -50,3 +28,14 @@ vim.keymap.set("n", "q", "<Nop>", { noremap = true, silent = true })
 vim.keymap.set("n", "<leader>acc", ":Codeium Chat<CR>", { desc = "Codeium Chat" })
 vim.keymap.set("n", "<leader>act", ":Codeium Toggle<CR>", { desc = "Codeium Toggle" })
 vim.keymap.set("n", "<leader>aca", ":Codeium Auth<CR>", { desc = "Codeium Auth" })
+
+-- Copy and Cut
+vim.keymap.set("n", "<C-c>", "+yy", opts)
+vim.keymap.set("v", "<C-c>", "+y")
+vim.keymap.set("n", "<C-x>", "+dd", opts)
+vim.keymap.set("v", "<C-x>", "+d")
+
+-- Paste
+vim.keymap.set("n", "<C-v>", '"+p', opts)
+vim.keymap.set("v", "<C-v>", '"+p', opts)
+vim.keymap.set("i", "<C-v>", "<C-r>+", opts)
