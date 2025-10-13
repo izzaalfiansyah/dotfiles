d820918572bab1ecf460829e436f196c64a52422
Muhammad Izza Alfiansyah <iansyah724@gmail.com>
2025-10-13 16:55:30 +0700

    back to catppuccin themes

--- .config/nvim/lua/plugins/color-scheme.lua
+++ .config/nvim/lua/plugins/color-scheme.lua
@@ -5,20 +5,6 @@ return {
       colorscheme = "catppuccin",
     },
   },
-  -- {
-  --   "projekt0n/github-nvim-theme",
-  --   name = "github-theme",
-  --   lazy = false, -- make sure we load this during startup if it is your main colorscheme
-  --   priority = 1000, -- make sure to load this before all the other start plugins
-  --   transparent = true,
-  --   config = function()
-  --     require("github-theme").setup({
-  --       -- ...
-  --     })
-  --
-  --     vim.cmd("colorscheme github_dark")
-  --   end,
-  -- },
   { "ellisonleao/gruvbox.nvim", priority = 1000, config = true },
   {
     "catppuccin/nvim",
