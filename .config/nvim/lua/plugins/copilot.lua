6205a5176b1c645130f314fdcf39c8de920d97ae
Muhammad Izza Alfiansyah <iansyah724@gmail.com>
2025-12-04 15:06:41 +0700

    add ai copilot

---
+++ .config/nvim/lua/plugins/copilot.lua
@@ -0,0 +1,10 @@
+return {
+  "zbirenbaum/copilot.lua",
+  cmd = "Copilot",
+  build = ":Copilot auth",
+  event = "InsertEnter",
+  opts = {
+    suggestion = { enabled = true },
+    panel = { enabled = true },
+  },
+}
