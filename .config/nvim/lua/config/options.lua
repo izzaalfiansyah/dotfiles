a6e0e6577319387437e874c1b782f2c9aaffac59
Muhammad Izza Alfiansyah <iansyah724@gmail.com>
2026-01-05 20:39:28 +0700

    feat (nvim): biome

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -9,6 +9,7 @@ vim.g.lazyvim_php_lsp = "intelephense"
 vim.g.lazyvim_python_lsp = "pyright"
 vim.g.lazyvim_python_ruff = "ruff"
 vim.g.lazyvim_picker = "fzf"
+vim.g.lazyvim_prettier_needs_config = true
 -- vim.g.codeium_disable_bindings = 1
 
 opt.wrap = true
