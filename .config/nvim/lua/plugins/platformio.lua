3655bb5a4f36489b144269bfcfe25d22a59ea7be
Muhammad Izza Alfiansyah <iansyah724@gmail.com>
2026-08-18 22:15:43 +0700

    feat(nvim): add platformio for iot execution

---
+++ .config/nvim/lua/plugins/platformio.lua
@@ -0,0 +1,12 @@
+return {
+  "anurag3301/nvim-platformio.lua",
+
+  dependencies = {
+    { "nvim-telescope/telescope.nvim" },
+    { "nvim-telescope/telescope-ui-select.nvim" },
+    { "nvim-lua/plenary.nvim" },
+    { "folke/which-key.nvim" },
+    { "nvim-treesitter/nvim-treesitter" },
+    { "akinsho/toggleterm.nvim" },
+  },
+}
